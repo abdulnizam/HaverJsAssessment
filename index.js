369b3c0c5583ac9c8ff55dc6233e46afc6219272
Abdul Nizam <abdulnizam@freniz.com>
2018-08-14 14:25:07 +0400

    updatd the code

--- index.js
+++ index.js
@@ -3,34 +3,43 @@ import fs from 'fs';
 
 // console.log(lib.runProgram('fourB'));
 
-const results = lib.runProgram('fourB');
+const results = lib.runProgram('one');
 
 let final_r = [ ];
 
-results.then(res => {
-	// write into file
- 	fs.writeFile("/tmp/test", res, err => {
-	    if(err) {
-	        return console.log(err);
-	    }
+// write into file
+fs.writeFile("/tmp/test", results, err => {
+	if(err) {
+	    return console.log(err);
+	}
 
-	    console.log("The file was saved!");
-	}); 
-})
+	console.log("The file was saved!");
+}); 
+
+// results.then(res => {
+// 	// write into file
+//  	fs.writeFile("/tmp/test", res, err => {
+// 	    if(err) {
+// 	        return console.log(err);
+// 	    }
+
+// 	    console.log("The file was saved!");
+// 	}); 
+// })
 
 // http endpoint
 
-import http from 'http';
+// import http from 'http';
 
-const options = {
-  hostname: '127.0.0.1',
-  port: 8080,
-  path: '/length_request'
-};
+// const options = {
+//   hostname: '127.0.0.1',
+//   port: 8080,
+//   path: '/length_request'
+// };
 
-// Make a request
-const req = http.request(options);
-req.end();
-req.on('information', (res) => {
-  console.log(`Got information prior to main response: ${res.statusCode}`);
-});
+// // Make a request
+// const req = http.request(options);
+// req.end();
+// req.on('information', (res) => {
+//   console.log(`Got information prior to main response: ${res.statusCode}`);
+// });
